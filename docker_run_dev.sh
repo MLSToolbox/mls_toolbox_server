02faf8917469b58020e0b1a851f3997dbc18ef59
Lidia <lidix91@users.noreply.github.com>
2025-12-03 19:24:29 +0100

    Change port mapping from 5000 to 5001

--- docker_run_dev.sh
+++ docker_run_dev.sh
@@ -1,1 +1,1 @@
-docker run -d -p 5000:5000 --network mls-network --rm --name mls_toolbox_server mls_toolbox_server
+docker run -d -p 5001:5000 --network mls-network --rm --name mls_toolbox_server mls_toolbox_server
